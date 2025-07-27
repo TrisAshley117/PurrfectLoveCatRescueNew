f8b6aec65926856b04fc46202382f58a7584c88d
TrisAshley <Tashley117@outlook.com>
2025-07-26 21:21:55 -0500

    TRISS-133_Home-Page-Skeleton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,6 +12,19 @@ const Home = () => {
                     At Purrfect Love Cat Rescue, our mission is to end the suffering and overpopulation of feral, abandoned, and abused cats through rescue and rehabilitation and through a Trap-Neuter-Return program with superior colony care, community education, and responsible adoption.
                 </div>
             </div>
+            <div className="BodyContainer">
+                <div className="MainContent">
+                    <div className="PhotoGalleryContainer">
+                        Photo Gallery Placeholder
+                    </div>
+                    <div className="RealityCheck">
+                        Reality Check Placeholder
+                    </div>
+                </div>
+                <div className="EventsContainer">
+                    Events Placeholder
+                </div>
+            </div>
         </div>
     )
 }
