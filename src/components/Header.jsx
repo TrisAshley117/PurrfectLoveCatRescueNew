fdfbca155ed08ed8045a39b9b3078b8a50bc6e70
TrisAshley <Tashley117@outlook.com>
2025-07-26 20:15:54 -0500

    Basic layout and navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import {React, useState} from 'react'
+import '../styles/Header.css'
+import Navigation from '../components/Navigation.jsx'
+
+const Header = () => {
+
+const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <div className='HeaderContainer'>
+            <div className="LogoNameContainer">
+                <img src='/src/assets/Logo.jpg' alt='Purrfect Love Cat Rescue logo' className='Logo' />
+                <b><span className="SiteName">Purrfect Love Cat Rescue</span></b>
+            </div>
+
+            <button className="HamburgerButton" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </button>
+
+            <div className={`NavigationContainer ${menuOpen ? 'open' : ''}`}>
+                <div className='Navigatable'>
+                    <b>Home</b>
+                </div>                           
+                <div className='Navigatable'>
+                    <b>About</b>
+                </div>
+              <div className='Navigatable'>
+                    <b>Donate</b>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
