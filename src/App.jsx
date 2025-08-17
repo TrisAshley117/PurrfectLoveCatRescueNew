10ab64880c83c4be31039df239f13263bda906b8
TrisAshley <Tashley117@outlook.com>
2025-08-17 17:17:23 -0500

    TRISS-139_Change-Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './App.css'
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx'
 import Home from './components/Home.jsx';
+import Donate from './components/Donate.jsx';
 
 function App() {
+
+  const [activePage, setActivePage] = useState("Home")
+
+  const NavigateToPage = (NewPageKey) => {
+      if (NewPageKey != activePage)
+      {
+        setActivePage(NewPageKey);
+      }
+  }
 
   return (
     <>
       <div className="app">
-        <Header />
+        <Header NavigateCommand={NavigateToPage} />
         <div className="Main">
-          <Home className="HomePage" />
+          {
+            activePage === "Home" ? <div className="HomePage"><Home /></div> : <div className="HomePage Invisible"><Home /></div>
+          }
+          {
+            activePage === "Donate" ? <div className="DonatePage"><Donate /></div> : <div className="DonatePage Invisible"><Donate /></div>
+          }
         </div>
         <Footer />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,14 @@ import '../styles/Header.css'
 import Navigation from '../components/Navigation.jsx'
 import logo from '../assets/Logo.jpg'
 
-const Header = () => {
+const Header = ({NavigateCommand}) => {
 
-const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const NavigatePage = (NewPageKey) => {
+        setMenuOpen(false);
+        NavigateCommand(NewPageKey);
+    }
 
     return (
         <div className='HeaderContainer'>
@@ -19,13 +24,13 @@ const [menuOpen, setMenuOpen] = useState(false);
             </button>
 
             <div className={`NavigationContainer ${menuOpen ? 'open' : ''}`}>
-                <div className='Navigatable'>
+                <div className='Navigatable' onClick={() => NavigatePage("Home")}>
                     <b>Home</b>
                 </div>                           
                 <div className='Navigatable'>
                     <b>About</b>
                 </div>
-              <div className='Navigatable'>
+              <div className='Navigatable' onClick={() => NavigatePage("Donate")}>
                     <b>Donate</b>
                 </div>
             </div>
