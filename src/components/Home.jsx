ef25ec37c4befb6502645438828ff32ef623c742
TrisAshley <Tashley117@outlook.com>
2026-02-19 17:43:15 -0600

    final-changes-before-review

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -111,14 +111,17 @@ const Home = () => {
                                 <b>Adoption Information</b>
                         </div>
                         <ul className="Events">
-                            <li>Weekly adoptions held at Pet Smart in Hoover, AL</li>
-                            <li>Come meet & hold cats & kittens!</li>
-                            <li>Adoption forms available to fill out in store</li>
+                            <li>Weekly adoptions held at Pet Smart in Hoover!</li>
+                            <ul className="SubEvents">
+                                <li className="SubEvent">3770 Riverchase Village Ste 3 Hoover, AL 35244</li>
+                                <li className="SubEvent">Come meet & hold cats & kittens!</li>
+                                <li className="SubEvent">Adoption forms available to fill out in store</li>
+                            </ul>
                             <li>Adoption Hours:</li>
                             <ul className="SubEvents">
-                                <li><b>Friday:</b> 2:30 PM - 5:00 PM</li>
-                                <li><b>Saturday:</b> 1:30 PM - 4:30 PM</li>
-                                <li><b>Sunday:</b> 1:00 PM - 4:30 PM</li>
+                                <li className="SubEvent"><b>Friday:</b> 2:30 PM - 5:00 PM</li>
+                                <li className="SubEvent"><b>Saturday:</b> 1:30 PM - 4:30 PM</li>
+                                <li className="SubEvent"><b>Sunday:</b> 1:00 PM - 4:30 PM</li>
                             </ul>
                         </ul>
                     </div>
